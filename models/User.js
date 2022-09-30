677c922b2d9390693405f70272f34267d34c4709
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-30 10:54:14 -0400

    initial commit - User model and broad project outline created

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose')
+const dateFormat = require('.utils/dateFormat')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/, 'Must match an email address!']
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
